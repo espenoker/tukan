49a38cc1a45f19dc7e267e4e3922c7827eeca265
espenoker <espen@studio-oker.com>
2021-04-20 09:34:31 +0200

    resize and canvas size

--- canvas.js
+++ canvas.js
@@ -1,6 +1,24 @@
 const canvas = document.querySelector("canvas")
 const sandbox = new GlslCanvas(canvas)
 
+const calcSize = function(){
+    let ww = window.innerWidth
+    let wh = window.innerHeight
+    let dpi = window.devicePixelRatio
+  
+    let s = Math.max(wh, ww)
+  
+    canvas.width = s * dpi
+    canvas.height = s * dpi
+  
+    canvas.style.width = s + "px"
+    canvas.style.height = s + "px"
+  
+  }
+  calcSize()
+  window.addEventListener("resize", function(){
+    calcSize()
+  })
 
 sandbox.load(frag)
 sandbox.setUniform("disp", "displacement4.png")
